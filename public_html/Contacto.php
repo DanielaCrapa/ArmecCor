4cd6d3f22201975f2c6566aa92cbf500257abeec
DaniiCD <danielacrapa@gmail.com>
2022-04-15 19:51:35 -0300

    commit1

---
+++ public_html/Contacto.php
@@ -0,0 +1,27 @@
+<?php
+$nombre= $_POST['nombre'];
+$mensaje= $_POST['mensaje'];
+$email= $_POST['email'];
+$destino = $_REQUEST['recipient'];
+$subject = $_REQUEST['subject'];
+$redirect = $_REQUEST['redirect'];
+
+
+
+if ($nombre != '') {
+$header = "From: $nombre <$email>\r\n";
+}
+else {
+$header = "From: $email\r\n";
+}
+$header .= "Return-Path: $email\r\n";
+$nombre = "Nombre: $nombre\r\n";
+$email ="Email: $email\r\n";
+$mensaje ="mensaje: $mensaje\r\n";
+
+
+mail($destino, $subject, $mensaje, $header);
+header('Location: ' . $redirect);
+?>
+
+
